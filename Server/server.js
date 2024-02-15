b2f8f61ed98807c8de3d6551ab82f92e40249470
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2024-02-15 21:04:42 +0100

    Set packages

--- Server/server.js
+++ Server/server.js
@@ -1,2 +1,20 @@
 import 'dotenv/config'
+import { ChatOpenAI } from "@langchain/openai"
+
+const model = new ChatOpenAI({
+    azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+    azureOpenAIApiVersion: process.env.OPENAI_API_VERSION,
+    azureOpenAIApiInstanceName: process.env.INSTANCE_NAME,
+    azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
+})
+
+/**
+ * Await answer
+ * const joke = await model.invoke("Tell me a Javascript joke!")
+ *
+ *
+ *
+ *
+ * */
+
 console.log(process.env.AZURE_OPENAI_API_KEY)
