5a4bdcaeaa32baa5d3a7f7b3d8631f1c704240e1
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2024-02-18 00:09:49 +0100

    Fixed Authentication middleware CORS
    Cuz they needed that apparently

--- Client/src/pages/NoteSelector.svelte
+++ Client/src/pages/NoteSelector.svelte
@@ -1,5 +1,5 @@
 <script>
-    import {logedin} from "../lib/auth.js";
+    import {logedin, token} from "../lib/auth.js";
     import {navigate} from "svelte-routing";
     import {lurl} from "../lib/url.js";
 
@@ -8,7 +8,14 @@
     if(!$logedin)
         navigate("/SignIn", {replace:true})
     const getNotes = async ()=>{
-        const response = await fetch(`${lurl}/api/notes/`)
+        const response = await fetch(`${lurl}/api/notes/`, {
+            method: "GET",
+            headers: { 'Content-Type': 'application/json' , 'Authorization': `Bearer ${$token}`},
+        })
+        let data = response.json()
+        console.log(data)
+        return data
+        //return response.json()
     }
 
     let notesPromise = getNotes()
@@ -17,5 +24,10 @@
 
 {#await notesPromise}
     {:then notes}
+    {#each notes as note}
+        <div>
+            {note.Note.name}
+        </div>
+    {/each}
     {:catch error}
 {/await}
--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import User from "../Models/User.js";
 
 const authenticate = async (req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
@@ -18,6 +20,7 @@ const authenticate = async (req, res, next) => {
         req.user = user;
         next();
     } catch (error) {
+        console.log(`Invalid Token ${token}`)
         res.status(401).json({ message: 'Invalid token',
         error});
     }
