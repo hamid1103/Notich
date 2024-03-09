6c43739635c044539c31ed5ee45ea4ced80eb5dd
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2024-03-09 18:29:15 +0100

    added https Support

--- Server/server.js
+++ Server/server.js
@@ -11,19 +11,43 @@ import {runSIO} from "./Controllers/Workspace.js";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as path from 'path';
+import fs from "fs";
+import https from "https";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express()
-const server = http.createServer(app)
-runSIO(server)
-
 const port = process.env.PORT | 3000
 
-server.listen(port, ()=>{
-    console.log("HTTP Server running with expressjs... Connecting to DB")
-})
+let server;
+if(process.env.privatekey){
+    const privateKey = fs.readFileSync(process.env.privatekey,'utf8')
+    const certificate = fs.readFileSync(process.env.certificate, "utf8")
+    const chain = fs.readFileSync(process.env.chain, "utf8")
+    console.log("got files")
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: chain
+    };
+    var httpServer = http.createServer(app)
+    server = https.createServer(credentials, app)
 
+    httpServer.listen(80, ()=>{
+        console.log("HTTP Server running with expressjs... Connecting to DB")
+    })
+    server.listen(443, ()=>{
+        console.log("HTTPS Server running with expressjs... Connecting to DB")
+    })
+
+}else {
+    server = http.createServer(app)
+    server.listen(port, ()=>{
+        console.log("HTTP Server running with expressjs... Connecting to DB")
+    })
+}
+
+runSIO(server)
 connectDB()
 
 app.use(function (req, res, next) {
