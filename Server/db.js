f84ee832bc95dfc5220bbf3fae498a3c0f12b848
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2024-03-09 18:03:14 +0100

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -2,12 +2,24 @@ import mongoose from "mongoose";
 import 'dotenv/config'
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.log('Error connecting to MongoDB:', error);
+    if(process.env.CMDBPW)
+    {
+        let pw = encodeURIComponent(process.env.CMDBPW)
+        try {
+            await mongoose.connect(`mongodb+srv://corvo:${pw}@prg8.jvfkdbs.mongodb.net/?retryWrites=true&w=majority&appName=PRG8`)
+            console.log('Connected to MongoDB cloud');
+        } catch (error) {
+            console.log('Error connecting to MongoDB:', error);
+        }
+    }else{
+        try {
+            await mongoose.connect(process.env.MONGODB_URI);
+            console.log('Connected to MongoDB');
+        } catch (error) {
+            console.log('Error connecting to MongoDB:', error);
+        }
     }
+
 };
 
 export { connectDB }
